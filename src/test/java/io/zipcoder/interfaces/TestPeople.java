acc978ea2d1f57a95fc95d27d342808251a61bc9
MannyMonocle <eetumadu@gmail.com>
2019-11-25 15:38:03 -0500

    TestPeople added

--- src/test/java/io/zipcoder/interfaces/TestPeople.java
+++ src/test/java/io/zipcoder/interfaces/TestPeople.java
@@ -14,4 +14,29 @@ public class TestPeople {
 
         Assert.assertTrue(supernovas.contains(luffy));
     }
+
+    @Test
+    public void testRemove(){
+        People supernovas = new People();
+        Person luffy = new Person(1,"Monkey D. Luffy");
+
+        supernovas.add(luffy);
+        supernovas.remove(luffy);
+
+        Assert.assertFalse(supernovas.contains(luffy));
+    }
+
+    @Test
+    public void testFindById() throws Exception{
+        People supernovas = new People();
+        Person luffy = new Person(1,"Monkey D. Luffy");
+        Person kid = new Person(2, "Eustass Kid");
+
+        supernovas.add(luffy);
+        supernovas.add(kid);
+
+        Person actual = supernovas.findById(1);
+
+        Assert.assertEquals(luffy,actual);
+    }
 }
