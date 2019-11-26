2bea6174c0aa5f6faffc87f7072e87ea6c8993cc
MannyMonocle <eetumadu@gmail.com>
2019-11-26 17:37:07 -0500

    People changed to generic

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -4,9 +4,10 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class People implements Iterable{
+public abstract class People<E extends  Person> implements Iterable{
+
     //Field
-    private ArrayList<Person> personList;
+    private ArrayList<E> personList;
 
     //Constructor
     public People(){
@@ -14,27 +15,27 @@ public class People implements Iterable{
     }
 
     //Methods
-    public void add(Person pirate){
+    public void add(E pirate){
         personList.add(pirate);
     }
 
-    public Person findById (long id) throws Exception {
-        for(Person pirate : personList){
+    public E findById (long id) throws Exception {
+        for(E pirate : personList){
             if(pirate.getId() == id){ return pirate; }
         }
         throw new Exception("No such Person In List");
     }
 
-    public Boolean contains(Person person){
+    public Boolean contains(E person){
         return personList.contains(person);
     }
 
-    public void remove(Person person){
+    public void remove(E person){
         personList.remove(person);
     }
 
     public void remove(long id){
-        for(Person pirate : personList){
+        for(E pirate : personList){
             if(pirate.getId() == id){ personList.remove(pirate); }
         }
     }
@@ -45,11 +46,15 @@ public class People implements Iterable{
 
     public Integer count(){ return personList.size(); }
 
+    abstract E[] toArray();
+
+    /*
     public Person[] toArray(){
         Person[] pirates = new Person[personList.size()];
         for(int i = 0; i < pirates.length; i++){ pirates[i] = personList.get(i); }
         return pirates;
     }
+    */
 
     @Override
     public Iterator iterator() {
